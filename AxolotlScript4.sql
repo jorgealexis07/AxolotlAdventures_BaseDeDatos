e37af62031b9fc656dba2493485752d1ab8c13a4
valduque <119557788+valduque@users.noreply.github.com>
2023-01-27 16:13:04 -0600

    Update AxolotlScript4.sql

--- AxolotlScript4.sql
+++ AxolotlScript4.sql
@@ -5,33 +5,33 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema db_axolotl
+-- Schema mydb
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema db_axolotl
+-- Schema mydb
 -- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
 
-CREATE SCHEMA IF NOT EXISTS `db_axolotl` DEFAULT CHARACTER SET utf8 ;
-USE `db_axolotl` ;
-
 -- -----------------------------------------------------
--- Table `db_axolotl`.`Actividades`
+-- Table `mydb`.`Actividades`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_axolotl`.`Actividades` (
+CREATE TABLE IF NOT EXISTS `mydb`.`Actividades` (
   `id_Actividades` INT NOT NULL AUTO_INCREMENT,
   `nom_actv` VARCHAR(45) NOT NULL,
   `precio_actv` INT NOT NULL,
-  `descrip_actv` VARCHAR(200) NOT NULL,
-`img_actv` VARCHAR(45) NOT NULL,
+  `descrip_actv` VARCHAR(300) NOT NULL,
+  `img_actv` VARCHAR(300) NOT NULL,
+  `Actividadescol` VARCHAR(45) NULL,
   PRIMARY KEY (`id_Actividades`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_axolotl`.`Clientes`
+-- Table `mydb`.`Clientes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_axolotl`.`Clientes` (
+CREATE TABLE IF NOT EXISTS `mydb`.`Clientes` (
   `id_Clientes` INT NOT NULL AUTO_INCREMENT,
   `nom_cliente` VARCHAR(45) NOT NULL,
   `app_cliente` VARCHAR(45) NOT NULL,
@@ -43,11 +43,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_axolotl`.`Compras`
+-- Table `mydb`.`Compras`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_axolotl`.`Compras` (
+CREATE TABLE IF NOT EXISTS `mydb`.`Compras` (
   `id_ComprasClientes` INT NOT NULL AUTO_INCREMENT,
-  `date_Venta` DATE NOT NULL,
+  `date_Compras` DATE NOT NULL,
   `numpx_actv` INT NOT NULL,
   `precio_total` INT NULL,
   `Clientes_id_Clientes` INT NOT NULL,
@@ -57,17 +57,16 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db_axolotl`.`Actividades_has_Compras`
+-- Table `mydb`.`Actividades_has_Compras`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_axolotl`.`Actividades_has_Compras` (
+CREATE TABLE IF NOT EXISTS `mydb`.`Actividades_has_Compras` (
   `Actividades_id_Actividades` INT NOT NULL,
   `Compras_id_ComprasClientes` INT NOT NULL,
   `Compras_Clientes_id_Clientes` INT NOT NULL,
-  PRIMARY KEY (`Actividades_id_Actividades`, `Ventas_id_VentasClientes`, `Ventas_Clientes_id_Clientes`),
-  INDEX `fk_Actividades_has_Ventas_Ventas1_idx` (`Ventas_id_VentasClientes` ASC, `Ventas_Clientes_id_Clientes` ASC) VISIBLE,
-  INDEX `fk_Actividades_has_Ventas_Actividades1_idx` (`Actividades_id_Actividades` ASC) VISIBLE)
+  PRIMARY KEY (`Actividades_id_Actividades`, `Compras_id_ComprasClientes`, `Compras_Clientes_id_Clientes`),
+  INDEX `fk_Actividades_has_Compras_Compras1_idx` (`Compras_id_ComprasClientes` ASC, `Compras_Clientes_id_Clientes` ASC) VISIBLE,
+  INDEX `fk_Actividades_has_Compras_Actividades1_idx` (`Actividades_id_Actividades` ASC) VISIBLE)
 ENGINE = InnoDB;
-
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
